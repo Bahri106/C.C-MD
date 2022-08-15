3b79c8c1fe4f9d37299ddddcec0340e9b4d1e5d7
Bahri106 <110438429+Bahri106@users.noreply.github.com>
2022-08-16 01:05:37 +0700

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,19 @@
+const qrku = "https://telegra.ph/file/f4ee3c55fd67cbd3ea861.jpg"
+
+let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
+╭─「 Donasi • Dana 」
+│ • im3/pulsa [6285853570297]
+│ • Dana  [6285707645429]
+╰────
+╭─「 *NOTE* 」
+│ > Ingin donasi? Wa.me/6285853570297
+│ _Hasil donasi akan digunakan buat sewa_
+│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ _24jam tanpa kendala_
+╰────
+`.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['about']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
